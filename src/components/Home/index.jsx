722c5ec627e6466daa7611b74f4d2d99cc85ea18
Luis Manuel López <compaluis51@hotmail.com>
2022-09-04 13:02:17 -0500

    peticion fetch correcta

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,64 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import Products from "../Products";
-import styles from './styles.module.scss'
+import { Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./home.css";
 
 const Home = () => {
+  const [ProductsData, setProductsData] = useState([]);
+
+  const peticionGet = () => {
+    fetch("https://corebiz-test.herokuapp.com/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProductsData(data);
+      });
+  };
+  useEffect(() => {
+    peticionGet();
+  }, []);
+
   return (
-    <div className={styles.home}>
+    <div className="products">
+      <h1>Más Vendidos</h1>
       
-      <Products />
+      <hr />
+      <div className="container_products ">
+        <Swiper className="swiper_productos "
+          modules={[Navigation, Pagination]}
+          navigation
+          loop="false"  
+          breakpoints={{
+            0: {
+              slidesPerView: 2,
+              spaceBetween: 2,
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 2,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 2,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 2,
+            },
+          }}
+          
+          
+        >
+          {ProductsData.map((product, i) => (
+            <SwiperSlide key={i}>
+              <Products product={product} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,25 +1,60 @@
 import React, { useContext } from "react";
 import CartContext from "../../context/CartContext";
-import styles from "./styles.module.scss";
-import { ProductsData } from "../../data/Data";
+//import styles from "./styles.module.scss";
+import accounting from "accounting";
+import "./Products.css";
 
-const Products = () => {
+const Products = ({ product }) => {
   /* Traemos del context la funcion para agregar un producto */
   const { AddItemToCart } = useContext(CartContext);
-  
+
+  const { imageUrl, productName, stars, listPrice, price, installments } =
+    product;
+
   return (
-    <div className={styles.productsContainer}>
-      {ProductsData.map((product, i) => (
-        <div key={i} className={styles.product}>
-          <img src={product.img} alt={product.name} />
-          <div>
-            <p>
-              {product.name} - ${product.price}
-            </p>
+    <div className="main">
+      <article className="container-products">
+        <div className="product">
+          <div className="imgproduct">
+            <img src={imageUrl} alt="pic" />
           </div>
-          <button onClick={() => AddItemToCart(product)}>Add to Cart</button>
+          <div className="container-hover">
+            <div className="name">
+              <h6>{productName}</h6>
+              <div className="rating">
+                {Array(stars)
+                  .fill()
+                  .map((_, i) => (
+                    <span>&#9733;</span>
+                  ))}
+              </div>
+              <p>
+                <br />
+                {listPrice && (
+                  <span className="listprice">de $ {listPrice}</span>
+                )}
+                <br />
+              </p>
+              <p className="price">
+                por {accounting.formatMoney(parseFloat(price))}
+              </p>
+
+              {installments[0] && (
+                <span className="installments">
+                  {" "}
+                  o en {installments[0]?.quantity} X de R${" "}
+                  {installments[0]?.value}
+                </span>
+              )}
+            </div>
+            <div className="btn-shop">
+              <button className="btn" onClick={() => AddItemToCart(product)}>
+                COMPRAR
+              </button>
+            </div>
+          </div>
         </div>
-      ))}
+      </article>
     </div>
   );
 };
